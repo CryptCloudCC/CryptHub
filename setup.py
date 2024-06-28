6b4d948297198616fac20c31607dbe1e7e61272c
cam <cammclain@skiff.com>
2024-06-28 18:27:26 -0500

    adding components constants

--- setup.py
+++ setup.py
@@ -1,2 +1,56 @@
 import os
 import subprocess
+
+
+CRYPTHUB_COMPONENTS: dict = {
+    
+    ## CryptUI ##
+    "CryptUI": {
+        "name": "CryptUI",
+        "description": "CryptHub's User Interface system built using Deno and React.",
+        "url": "https://github.com/CryptCloudCC/CryptUI"
+        },
+    
+    ## CryptDB ##
+    "CryptDB": {
+        "name": "CryptDB",
+        "description": "CryptHub's database management system.",
+        "url": "https://github.com/CryptCloudCC/CryptDB"
+        },
+    
+    ## CryptBucket ##
+    "CryptBucket": {
+        "name": "CryptBucket",
+        "description": "CryptHub's storage solution based on Amazon S3.",
+        "url": "https://github.com/CryptCloudCC/CryptBucket"
+    },
+
+    ## CryptFlows ##
+    "CryptFlows": {
+        "name": "CryptFlows",
+        "description": "Task orchestrator handling workflow logic using Directed Acyclic Graphs (DAGs).",
+        "url": "https://github.com/CryptCloudCC/CryptFlows"
+    },
+
+    ## CryptComms ##
+    "CryptComms": {
+        "name": "CryptComms",
+        "description": "CryptHub's comprehensive communication system.",
+        "url": "https://github.com/CryptCloudCC/CryptComms"
+    },
+    
+    ## CryptAuth ##
+    "CryptAuth": {
+        "name": "CryptAuth",
+        "description": "CryptHub's authentication system.",
+        "url": "https://github.com/CryptCloudCC/CryptAuth"
+    }
+    
+}
+
+
+
+
+
+def main():
+    pass
