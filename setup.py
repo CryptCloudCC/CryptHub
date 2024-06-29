168e1fcbc85fc1e93233fa7be41763f0e2117c51
cam <cammclain@skiff.com>
2024-06-29 09:21:03 -0500

    sync

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ import subprocess
 
 
 CRYPTHUB_COMPONENTS: dict = {
-    
+
     ## CryptUI ##
     "CryptUI": {
         "name": "CryptUI",
         "description": "CryptHub's User Interface system built using Deno and React.",
         "url": "https://github.com/CryptCloudCC/CryptUI"
         },
-    
+
     ## CryptDB ##
     "CryptDB": {
         "name": "CryptDB",
         "description": "CryptHub's database management system.",
         "url": "https://github.com/CryptCloudCC/CryptDB"
         },
-    
+
     ## CryptBucket ##
     "CryptBucket": {
         "name": "CryptBucket",
@@ -38,22 +38,18 @@ CRYPTHUB_COMPONENTS: dict = {
         "description": "CryptHub's comprehensive communication system.",
         "url": "https://github.com/CryptCloudCC/CryptComms"
     },
-    
+
     ## CryptAuth ##
-    "CryptAuth": {
-        "name": "CryptAuth",
-        "description": "CryptHub's authentication system.",
-        "url": "https://github.com/CryptCloudCC/CryptAuth"
-    }
-    
+
+
 }
 
 def clone_component_repos_to_components_directory():
-    
+
     ## Establish the relative path of the components directory
     ## `CryptHub-Platform/CryptHub-Components`
     CRYPTHUB_COMPONENTS_DIRECTORY: str = "CryptHub-Platform/CryptHub-Components"
-    
+
     for component in CRYPTHUB_COMPONENTS:
         subprocess.run(f"git clone {CRYPTHUB_COMPONENTS[component]['url']}", shell=True, cwd=CRYPTHUB_COMPONENTS_DIRECTORY)
         subprocess.run(f"cd {CRYPTHUB_COMPONENTS_DIRECTORY}/{CRYPTHUB_COMPONENTS[component]['name']} && git checkout {CRYPTHUB_COMPONENTS[component]['branch']}", shell=True, cwd=CRYPTHUB_COMPONENTS_DIRECTORY)
